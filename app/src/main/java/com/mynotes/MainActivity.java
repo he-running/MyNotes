01c119a70c6a5a5af2f5f225cfe3c4bafca9c442
He <1035922783@qq.com>
2016-04-19 09:32:51 +0800

    第三次提交

--- app/src/main/java/com/mynotes/MainActivity.java
+++ app/src/main/java/com/mynotes/MainActivity.java
@@ -1,17 +1,42 @@
 package com.mynotes;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.SimpleCursorAdapter;
 
-public class MainActivity extends AppCompatActivity {
+import com.mynotes.db.NotesDB;
+
+public class MainActivity extends ListActivity {
+
+    private SimpleCursorAdapter cursorAdapter;
+    private NotesDB db;
+    private SQLiteDatabase dbRead;
+
+    public static final int REQUEST_CODE_ADD_NOTE = 1;
+    public static final int REQUEST_CODE_EDIT_NOTE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        cursorAdapter = new SimpleCursorAdapter(this, R.layout.list_item,
+                null, new String[]{NotesDB.COLUMN_NAME_NOTE_NAME, NotesDB.COLUMN_NAME_NOTE_DATE},
+                new int[]{R.id.tvName, R.id.tvDate});
 
+        setListAdapter(cursorAdapter);
+
+        refreshNotesListView();
 
 
     }
+
+    private void refreshNotesListView() {
+
+        cursorAdapter.changeCursor(dbRead.query(NotesDB.TABLE_NAME_NOTES,
+                null, null, null, null, null, null));
+    }
+
+
 }
